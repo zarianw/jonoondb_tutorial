ec37a59a7a81d038fa7330218cc43e153c6f2a8e
zarianw <zarianwaheed@live.com>
2016-12-22 02:55:54 -0800

    Fixing jonoondb folder name.

--- scripts/install_jonoondb_master.sh
+++ scripts/install_jonoondb_master.sh
@@ -1,5 +1,5 @@
 set -x
 set -e
-wget --output-document=jonoondb.tar.gz https://github.com/zarianw/jonoondb/archive/master.tar.gz >> output.txt
-tar xzf jonoondb.tar.gz > output.txt
-$(cd jonoondb && cmake . -G "Unix Makefiles" -DBOOST_ROOT=../boost_1_60_0/64bit && sudo make install)
+wget --output-document=jonoondb-master.tar.gz https://github.com/zarianw/jonoondb/archive/master.tar.gz >> output.txt
+tar xzf jonoondb-master.tar.gz > output.txt
+$(cd jonoondb-master && cmake . -G "Unix Makefiles" -DBOOST_ROOT=../boost_1_60_0/64bit && sudo make install)
