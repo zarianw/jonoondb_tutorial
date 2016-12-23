1f38a7f62549c5ddceb8b24094b3e97503455da5
zarianw <zarianwaheed@live.com>
2016-12-22 18:08:32 -0800

    Fixing build issue.

--- scripts/install_jonoondb_master.sh
+++ scripts/install_jonoondb_master.sh
@@ -2,4 +2,4 @@ set -x
 set -e
 wget --output-document=jonoondb-master.tar.gz https://github.com/zarianw/jonoondb/archive/master.tar.gz >> output.txt
 tar xzf jonoondb-master.tar.gz > output.txt
-$(cd jonoondb-master && cmake . -G "Unix Makefiles" -DBOOST_ROOT=../boost_1_60_0/64bit && sudo make install)
+$(cd jonoondb-master && cmake . -G "Unix Makefiles" -DBOOST_ROOT=../boost_1_60_0/64bit >> output.txt && sudo make install >> output.txt)
